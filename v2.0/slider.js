98508e4d2750da4047ce6689928e33bd7d7dc007
Thomas <t.pichegru@gmail.com>
2023-10-24 11:13:59 +0200

    Elegant Slider v3.0

--- v2.0/slider.js
+++ v2.0/slider.js
@@ -9,17 +9,16 @@ class Slider {
 	constructor(node, options) {
 		try {
 			this.wrapper = node;
-			this.slidesTape = node.querySelector(".slides-tape");
-			this.animation = this.slidesTape.classList.contains("fade-transition") ? "fade" : "translate";
+			this.animation = options.animation === "fade" ? "fade" : "translate";
 			this.animationTiming = options.animationTiming || 200;
 			this.pauseTiming = options.pauseTiming || 0;
-			this.position = 1;
+			this.position = parseInt(options.initialPosition) || 1;
 			this.infiniteCycle = options.infiniteCycle || false;
 			this.showArrows = options.showArrows ?? true;
 			this.showDots = options.showDots === undefined ? true : options.showDots;
 
-			this.length = this.slidesTape.children.length;
-			this.children = Array.from(this.slidesTape.children);
+			this.length = this.wrapper.children.length;
+			this.children = Array.from(this.wrapper.children);
 
 			this.wrapper.addEventListener("touchstart", e => this.touchesX = e.touches[0].pageX, { passive: true })
 			this.wrapper.addEventListener("touchend", e => this.touchEnd(e, this), { passive: true })
@@ -33,25 +32,44 @@ class Slider {
 
 
 	buildSliderFrame() {
+
+		// Style Attributes of slider wrapper
+		this.wrapper.classList.add("sliderWrapper");
+		if (!["relative", "absolute", "fixed"].includes(getComputedStyle(this.wrapper).position)) { this.wrapper.style.position = "relative" }
 		this.wrapper.style.setProperty("--animation-duration", this.animationTiming + "ms")
-		// cloning first and last slides
-		let firstClone = this.children[0].cloneNode(true)
-		let lastClone = this.children[this.length - 1].cloneNode(true)
+
+		// Create slidesTape that contains all slides
+		this.slidesTape = create("div", "slidesTape")
+		if (this.animation === "fade") { this.slidesTape.classList.add("opacity-transition") }
+
+		// Wrap each firstchild element in a div.slide element
+		this.children.forEach(slideContent => {
+			let slide = create("div", "slide")
+			slide.append(slideContent);
+			this.slidesTape.appendChild(slide)
+		})
+
+		// cloning first slide to last position and last slide to first position
+		let firstClone = this.slidesTape.children[0].cloneNode(true)
+		let lastClone = this.slidesTape.children[this.slidesTape.children.length - 1].cloneNode(true)
 		this.slidesTape.appendChild(firstClone)
-		this.slidesTape.insertBefore(lastClone, this.children[0])
+		this.slidesTape.insertBefore(lastClone, this.slidesTape.children[0])
+
+		// Add content to wrapper
+		this.wrapper.appendChild(this.slidesTape)
 	}
 
 	generateCommands() {
 		if (this.showArrows) {
-			this.previousButton = create("div", "slider-button previous");
+			this.previousButton = create("div", "sliderButton previous");
 			this.previousButton.addEventListener("click", () => this.showSlide(this.position - 1))
-			this.nextButton = create("div", "slider-button next");
+			this.nextButton = create("div", "sliderButton next");
 			this.nextButton.addEventListener("click", () => this.showSlide(this.position + 1))
 			this.wrapper.append(this.nextButton, this.previousButton)
 		}
 
 		if (this.showDots) {
-			this.dotLine = create("div", "slide-dot-line")
+			this.dotLine = create("div", "slideDotLine")
 			for (let i = 1; i <= this.length; i++) {
 				let dot = document.createElement("div")
 				dot.addEventListener("click", () => { this.showSlide(i) })
@@ -72,8 +90,6 @@ class Slider {
 			if (this.position === 0) { activeDotIndex = this.length - 1; }
 			else if (this.position > this.length) { activeDotIndex = 0; }
 			else { activeDotIndex = this.position - 1; }
-
-			// Update active dot in the middle of the animation
 			setTimeout(() => {
 				Array.from(this.dotLine.children).forEach(dot => { dot.classList.remove("active") })
 				this.dotLine.children[activeDotIndex].classList.add("active");
@@ -96,7 +112,7 @@ class Slider {
 		if (this.animation === "fade") {
 			if (isAfterLastSlide) { this.position = 1 }
 			else if (isBeforeFirstSlide){ this.position = this.length }
-			this.children.forEach(slide => { slide.style.opacity = 0 })
+			Array.from(this.slidesTape.children).forEach(slide => { slide.style.opacity = 0 })
 			this.slidesTape.children[this.position].style.opacity = 1
 		}
 		else {
@@ -119,8 +135,8 @@ class Slider {
 
 	touchEnd(e, slider) {
 		let deltaX = slider.touchesX - e.changedTouches[0].pageX
-		if (deltaX >= 50) { slider.showSlide(slider.position + 1) }
-		else if (deltaX <= -50) { slider.showSlide(slider.position - 1) }
+		if (deltaX >= 25) { slider.showSlide(slider.position + 1) }
+		else if (deltaX <= -25) { slider.showSlide(slider.position - 1) }
 	}
 
 	initiate() {
